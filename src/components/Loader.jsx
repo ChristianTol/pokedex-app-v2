12201aab6a0b71166b20925b7134a3c85f28bead
ChristianTol <christian.tol1998@hotmail.com>
2022-12-13 17:35:34 +0100

    Loader component updated.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,10 @@
+import { DotSpinner } from "@uiball/loaders";
 import React from "react";
 
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="container-loader">
+      <DotSpinner size={40} speed={0.9} color="black" />
+    </div>
+  );
 };
